75147fbeb1866de8fff85e73fc3a1e360bc2f60a
Adil Demirleng <demirleng@gmail.com>
2021-08-29 16:33:54 -0400

    first commit

---
+++ Day01_Variables.js
@@ -0,0 +1,22 @@
+var myName; 
+console.log(myName)//By default, variables are undefined
+
+var myName='Javascript';
+console.log(myName); //"Javascript"
+
+var myName='Java';
+console.log(myName);
+
+myName=true;
+console.log(myName);
+
+myName=6;
+console.log(myName);
+
+var fName='John', lName='Cash';
+console.log(fName);
+console.log(lName);
+
+var firstName='Sam'
+var lastName='Walton'
+console.log(firstName +' '+lastName);//"Sam Walton"
